bb8035e7cb80b606caca0c058dd860ff29dc1f82
Alex Lapinski <contact@alexlapinski.name>
2014-04-19 11:03:30 -0400

    updated gitignore to ignore .class and finished parsing of cli arguments for client

--- edu/drexel/cs544/alexlapinski/ClientApplication.java
+++ edu/drexel/cs544/alexlapinski/ClientApplication.java
@@ -1,7 +1,34 @@
 package edu.drexel.cs544.alexlapinski;
 
+import java.lang.Integer;
+
 public class ClientApplication {
+
+    private static String _serverAddress;
+    private static int _fixedPort;
+    private static String _filenameToTransfer;
+
     public static void main(String [ ] args) {
-        System.out.println("I'm a Client!");      
+
+        if( args.length == 0 ) {
+            _printHelp();
+        } else {
+            _parseArguments(args);
+        }
+    }
+
+    private static void _printHelp() {
+        System.out.println("Use the following format to specify commands: <server_address> <f_port> <filename>");
+    }
+
+    private static void _parseArguments(String[] args) {
+        _serverAddress = args[0];
+        _fixedPort = Integer.parseInt(args[1], 10);
+        _filenameToTransfer = args[2];
+
+        System.out.println("Arguments Supplied");
+        System.out.println("     ServerAddress: " + _serverAddress);
+        System.out.println("     FixedPort: " + _fixedPort);
+        System.out.println("     FilenameToTransfer: " + _filenameToTransfer);
     }
 }
